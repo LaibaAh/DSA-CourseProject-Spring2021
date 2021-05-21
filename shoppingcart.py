2891c7a543656e0e0e4bc78008da75349fa086c4
Laiba Ahmed <la06855@st.habib.edu.pk>
2021-05-21 20:20:43 +0500

    New file updated

---
+++ shoppingcart.py
@@ -0,0 +1,8 @@
+inventory= {'Acrylic Shield': [1500, 50, "static\images\Acrylic Sheild.jpg"],
+     'Card-keychain': [60, 50, "static\images\card-keychain.jpg"], 'Classwiz-Calculator': [2000, 50, "static\images\Casio Classwiz.jpeg"],
+     'Color-changing-mug': [600, 50, "static\images\colour changing Mug.jpg"], 'Corrector': [10, 50, "image"], 'Eraser': [5, 50, "static\images\eraser.jpeg"],
+     'HU-spiral-Notebook': [500, 50, "static\images\HU-Spiral Notebook.jpg"], 'Markers': [250, 50, "static\images\markers.jpeg"],'Pen': [40, 50, "static\images\pen.png"], 'Pencil': [10, 50, "static\images\pencil.jpeg"], 
+     'Scale': [5, 50, "static\images\scale.jpeg"], 'Sharpner': [60, 50, "static\images\sharpner.jpeg"], 'Stapler': [60, 50, "static\images\stapelar.jpeg"], 'Tape': [60, 50, "static\images\Tape.jpeg"], 
+     'White-notepad':[100, 50, "static\images\White notepad.jpg"], 'World-map-penset': [600, 50, "static\images\world-map pen set.jpg"]}
+
+
