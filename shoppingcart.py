c003fcfaa1071c2e01f85d213202bc28a94008c7
Sijjil22 <83570391+Sijjil22@users.noreply.github.com>
2021-05-21 23:31:30 +0500

    Update shoppingcart.py

--- shoppingcart.py
+++ shoppingcart.py
@@ -4,5 +4,3 @@ inventory= {'Acrylic Shield': [1500, 50, "static\images\Acrylic Sheild.jpg"],
      'HU-spiral-Notebook': [500, 50, "static\images\HU-Spiral Notebook.jpg"], 'Markers': [250, 50, "static\images\markers.jpeg"],'Pen': [40, 50, "static\images\pen.png"], 'Pencil': [10, 50, "static\images\pencil.jpeg"], 
      'Scale': [5, 50, "static\images\scale.jpeg"], 'Sharpner': [60, 50, "static\images\sharpner.jpeg"], 'Stapler': [60, 50, "static\images\stapelar.jpeg"], 'Tape': [60, 50, "static\images\Tape.jpeg"], 
      'White-notepad':[100, 50, "static\images\White notepad.jpg"], 'World-map-penset': [600, 50, "static\images\world-map pen set.jpg"]}
-
-
